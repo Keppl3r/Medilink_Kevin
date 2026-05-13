30608710bb4c54dc8b2f76bdf7cd7107d2da2910
Keppl3r <kevin.mendoza215752@potros.itson.edu.mx>
2026-05-13 16:56:10 -0700

    Creando repo medilink

---
+++ Medilink_Kevin/negocio/src/main/java/com/negocio/Negocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.negocio;
+
+/**
+ *
+ * @author keppler
+ */
+public class Negocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
